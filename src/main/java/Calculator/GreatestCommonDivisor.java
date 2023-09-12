f77071cd5a7fabb9776ecb3d3e8fd80970c5c3b9
OMartsin <oleksandrmartsin@gmail.com>
2023-09-12 21:00:37 +0300

    delete gcd for bigIntegers(because bigInteger have gcd method)

--- src/main/java/Calculator/GreatestCommonDivisor.java
+++ src/main/java/Calculator/GreatestCommonDivisor.java
@@ -1,20 +1,11 @@
 package Calculator;
 
-import java.math.BigInteger;
-
 public class GreatestCommonDivisor {
     public static int gcd(int a, int b) {
         if (b == 0) {
             return a;
         } else {
             return gcd(b, a % b);
-        }
-    }
-    public static BigInteger gcd(BigInteger A, BigInteger B) {
-        if (B.equals(BigInteger.valueOf(0))) {
-            return A;
-        } else {
-            return gcd(B, A.remainder(B));
         }
     }
 }
