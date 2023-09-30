570aba71b7ad4b9804f27b31bf1e55a9f89c1fbb
OMartsin <oleksandrmartsin@gmail.com>
2023-09-30 21:40:50 +0300

    [feat] email validation without regex

--- src/main/java/EmailValidator.java
+++ src/main/java/EmailValidator.java
@@ -1,11 +1,33 @@
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class EmailValidator {
-    public final static String EMAIL_PATTERN = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
     public static boolean isValidEmail(String email) {
-        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
-        Matcher matcher = pattern.matcher(email);
-        return matcher.matches();
+        if (email == null || email.length() == 0) {
+            return false;
+        }
+
+        boolean hasAtSymbol = false;
+        boolean hasDot = false;
+        boolean hasSpace = false;
+
+        for (char c : email.toCharArray()) {
+            if (c == '@') {
+                if (hasAtSymbol || hasDot || hasSpace) {
+                    return false;
+                }
+                hasAtSymbol = true;
+            } else if (c == '.') {
+                if (hasDot || hasSpace) {
+                    return false;
+                }
+                hasDot = true;
+            } else if (c == ' ') {
+                if (hasSpace) {
+                    return false;
+                }
+                hasSpace = true;
+            }
+        }
+
+        return hasAtSymbol && hasDot && !hasSpace;
     }
+
 }
