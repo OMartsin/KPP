dfb0300ee8cc6025d68c2cf2299ed6c8cdf6339e
OMartsin <oleksandrmartsin@gmail.com>
2023-10-05 13:46:37 +0300

    [feat] add public holidays for test

--- src/main/java/HolidaysChecker.java
+++ src/main/java/HolidaysChecker.java
@@ -10,6 +10,13 @@ public class HolidaysChecker {
         publicHolidays = new HashSet<>();
         publicHolidays.add(MonthDay.of(1, 1));   // New Year's Day
         publicHolidays.add(MonthDay.of(12, 25)); // Christmas Day
+        publicHolidays.add(MonthDay.of(12, 26)); // Boxing Day
+        publicHolidays.add(MonthDay.of(5, 1));   // Labour Day
+        publicHolidays.add(MonthDay.of(5, 2));   // Labour Day
+        publicHolidays.add(MonthDay.of(5, 3));   // Labour Day
+        publicHolidays.add(MonthDay.of(8, 24));  // Independence Day
+        publicHolidays.add(MonthDay.of(10, 14)); // Defender of Ukraine Day
+        publicHolidays.add(MonthDay.of(6,28));  // Constitution Day
         // Add more public holidays as needed
     }
 
